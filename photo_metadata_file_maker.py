766c01f1499b7899fb9efd79664c0df0bff79294
Alistair <a.morrison@jhu.edu>
2018-12-22 22:37:28 -0500

    Iterate over image base names

--- photo_metadata_file_maker.py
+++ photo_metadata_file_maker.py
@@ -1,36 +1,57 @@
 '''
-This script walks through the specified directory, identifies tiff image files and
-creates a yaml side-car medatada file for each file base. The script populates the image file
-name and the image file creation date. The rest of the yaml file is a standard
-template.
+This script walks through the specified directory, identifies tiff or jpeg image files and
+creates a yaml side-car medatada file for unique each base name. If you have a tiff
+and a jpeg with the same name you get only one yaml file. The script populates the image file
+name and the image file creation date. The rest of the yaml file is hard coded in this script
 '''
+
 import os
 from pathlib import Path
 import imghdr
 
-md_elements = []
-
-def isImageExtension( ext ):
-    # Is the string ext a member of our set of image file extenstions?
-    ext_set = {'.tif', '.tiff', '.jpg', '.jpeg', '.png'}
-    return (ext.lower() in ext_set);
+valid_image_types = ['tiff', 'jpeg']
 
 # Set the folder path
-foldername = Path.home()  / 'Desktop/_Alistair/photos/scans'
+folderpath = Path.home()  / 'Desktop/_Alistair/photos/scans'
 
-(Path.exists(foldername))
-print(foldername)
+# Iterator that returns the base names of image files a directory
+def image_base_names(path):
+    for img in os.listdir(path):
+        img_path = os.path.join(path, img)
+        if os.path.isfile(img_path):
+            if (imghdr.what(img_path) in valid_image_types):
+                yield os.path.splitext(img)[0]
+
+# Create a set of the unique base names of the image files in the spcified directory
+image_set = set()
+if (Path.exists(folderpath)):
+    for img in image_base_names(folderpath):
+        image_set.add(img)
+
+
+for img in sorted(image_set):
+    print(img)
 
 '''
-foldername = Path(os.path.expanduser('~') + '\Desktop\_Alistair\photos\scans')
-print("Directory name is : " + PosixPath(foldername))
+        fparts =  os.path.splitext(img)
+        fname_set.add(fparts[0])
 
 '''
+    # Make a set of the unique file names - without extensions - for image files in the directory
 '''
-# Make a set of the unique file names - without extensions - for image files in the directory
-fname_set = set()
+
 for f in os.listdir(foldername):
+#    if is_valid_image(f.name):
     print(f)
+    print(imghdr.what(f))
+
+'''
+'''
+
+
+'''
+'''
+
     '''
 
 '''
