70d676730914f5d13424fd38ea3252375531b08b
Alistair <a.morrison@jhu.edu>
2018-12-24 09:19:41 -0500

    Datestamp check
    Added is_a_datestamp and started file rename logic

--- image_directory.py
+++ image_directory.py
@@ -2,7 +2,8 @@
 import os
 from pathlib import Path
 import imghdr
-
+import datetime
+import re
 
 class ImageDirectory:
 
@@ -10,6 +11,9 @@ class ImageDirectory:
         self.image_file_names = set()  # image file names with extensions
         self.image_names = set()    # image file names without extensions
         self.folder_path = Path.home()  / directory_name
+        self.dt_stamp = datetime.datetime.now().strftime("%Y-%m-%d") # date stamp for file renaming
+        self.name_map = {} # maps current image file names (without extension) to new names
+
         try:
             # only proceed if path is valid
             assert Path.exists(self.folder_path)
@@ -37,4 +41,25 @@ class ImageDirectory:
             yield img
 
     # replace image file names with date stamp plus counter
-#    def rename_image_files(self):
+    def rename_image_files(self, prefix):
+        ctr = 0
+        for img in self.image_names:
+            if not self.is_a_datestamp(img):
+                ctr += 1
+                self.name_map[img] = prefix + "_" + self.dt_stamp + "_" + f"{ctr:04}"
+        if self.name_map:
+            for ky, val in self.name_map.items():
+                print(ky,val)
+        else: print("There are no image files that do no already have names based on date stamp in", self.folder_path)
+        
+    # check that string contains a valid date in iso format
+    def is_a_datestamp(self, val):
+        match_obj = re.search( r'\d{4}-\d{2}-\d{2}', val)
+        if match_obj != None:
+            try:
+                match_string = match_obj.group()
+                test_date = datetime.datetime.strptime(match_string, "%Y-%m-%d")
+                return True
+            except ValueError:
+                return False
+        else: return False
