9e35bd1228823a0277dcea1908f2ed557155982d
Alistair <a.morrison@jhu.edu>
2018-12-25 11:31:23 -0500

    File rename logic
    Worked out file rename logic. Need to add actual renaming.

--- image_directory.py
+++ image_directory.py
@@ -8,7 +8,7 @@ import re
 class ImageDirectory:
 
     def __init__(self, directory_name):
-        self.image_file_names = set()  # image file names with extensions
+        self.image_files_attrs = {} # nested dictionary of image file names and their attributes
         self.image_names = set()    # image file names without extensions
         self.folder_path = Path.home()  / directory_name
         self.dt_stamp = datetime.datetime.now().strftime("%Y-%m-%d") # date stamp for file renaming
@@ -24,21 +24,20 @@ class ImageDirectory:
                 img_path = os.path.join(self.folder_path, img)
                 if os.path.isfile(img_path):
                     if (imghdr.what(img_path) != None): # has a valid image extension
-                        self.image_file_names.add(os.path.basename(img_path))
-                        self.image_names.add(os.path.splitext(img)[0])
-
+                        # build nested dictorary of image file names and attributes
+                        pref, suf = os.path.splitext(img)
+                        typ = imghdr.what(img_path)
+                        self.image_files_attrs[img] = {'prefix': pref, 'suffix': suf, 'img_type': typ}
+                        # build a set of unique image names
+                        self.image_names.add(pref)
         except AssertionError:
             print ("Error in class", self.__class__.__name__ , " -- Calling function provided an invalid path:", self.folder_path)
 
     # iterator for list of image file names with extensions
     def image_files(self):
-        for fl in self.image_file_names:
-            yield fl
+        for f in self.image_files_attrs.keys():
+            yield f
 
-    # iterator for list of image unique names without extensions
-    def unique_image_names(self):
-        for img in self.image_names:
-            yield img
 
     # replace image file names with date stamp plus counter
     def rename_image_files(self, prefix):
@@ -48,10 +47,15 @@ class ImageDirectory:
                 ctr += 1
                 self.name_map[img] = prefix + "_" + self.dt_stamp + "_" + f"{ctr:04}"
         if self.name_map:
-            for ky, val in self.name_map.items():
-                print(ky,val)
+            for old_file_name in self.image_files():
+                old_prefix = self.image_files_attrs[old_file_name]['prefix']
+                # since we skipped over files names that are already dates stamps do a check
+                if self.name_map.get(old_prefix, False):
+                    new_file_name = self.name_map[old_prefix] + self.image_files_attrs[old_file_name]['suffix']
+                    print(old_file_name + " => " + new_file_name)
+
         else: print("There are no image files that do no already have names based on date stamp in", self.folder_path)
-        
+
     # check that string contains a valid date in iso format
     def is_a_datestamp(self, val):
         match_obj = re.search( r'\d{4}-\d{2}-\d{2}', val)
--- tester.py
+++ tester.py
@@ -4,13 +4,3 @@ img = img_mod.ImageDirectory('Desktop/_Alistair/photos/scans')
 
 
 img.rename_image_files("DRM04")
-
-'''
-for nm in img.unique_image_names():
-    print(nm)
-
-
-for f in img.image_files():
-    print(f)
-
-    '''
