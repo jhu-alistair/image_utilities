3190cc6418e0168747601bfccc09a6620bf448c3
Alistair <a.morrison@jhu.edu>
2019-04-14 16:58:39 -0400

    Fixed util_list_files to use get_config

--- util_list_files.py
+++ util_list_files.py
@@ -1,7 +1,7 @@
 # List files in a directory. Useful for testing the path
 from local_tools import *
 from image_renamer import ImageRenamer
-img_path = get_path()
+img_path = get_config('path')
 fl = ImageRenamer(img_path)
 for ff in fl.image_files():
     print(ff)
