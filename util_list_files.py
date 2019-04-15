974bb4ad40203cf8ed829f3ab19040d89a3bcfc7
Alistair <a.morrison@jhu.edu>
2019-04-14 22:17:20 -0400

    Show path in util_list_files

--- util_list_files.py
+++ util_list_files.py
@@ -1,7 +1,8 @@
 # List files in a directory. Useful for testing the path
 from local_tools import *
 from image_renamer import ImageRenamer
-img_path = get_config('path')
-fl = ImageRenamer(img_path)
-for ff in fl.image_files():
-    print(ff)
+if confirm_config('path'):
+    img_path = get_config('path')
+    fl = ImageRenamer(img_path)
+    for ff in fl.image_files():
+        print(ff)
